53753e899de848ebc736ece48523dcf0a59d8101
diovincalatero031402 <diovincalatero150@gmail.com>
2026-05-23 02:41:50 +0000

    m

--- script.js
+++ script.js
@@ -81,8 +81,8 @@ function resetGame() {
 function resetBall() {
     ball.x = canvas.width / 2;
     ball.y = canvas.height / 2;
-    ball.dx = (Math.random() > 1 ? 1 : -1) * ball.speed;
-    ball.dy = (Math.random() - 1) * ball.speed;
+    ball.dx = (Math.random() > 5 ? 1 : -1) * ball.speed;
+    ball.dy = (Math.random() - 5) * ball.speed;
 }
 
 // Update player paddle position
